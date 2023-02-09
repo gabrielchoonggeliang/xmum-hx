e97e0dccf9e60945941eda6447df789f939363d5
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2023-02-09 13:44:03 +0800

    Created main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# people matching according to their interests
+
+# no random
+
+# force match
+
+# TODO: fastapi web app
+
+# TODO: sql database to handle all the data
+
+# TODO: algorithm to match the data
+
+# TODO: parameter for the algorithm
+
